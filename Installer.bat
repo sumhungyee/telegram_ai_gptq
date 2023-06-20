f3cfa07218e6ba68368a6b8ef9fa959a7d8e7474
HY <e0726431@u.nus.edu>
2023-06-20 15:35:50 +0800

    modify installer to support reinstallations allow users to reinstall corruptd files by rerunning the installer

--- Installer.bat
+++ Installer.bat
@@ -1,5 +1,6 @@
 
 @echo off
+setlocal enabledelayedexpansion
 set root=C:\Users\%USERNAME%\anaconda3\condabin
 if not exist %root% (
     echo Anaconda not installed! Please install anaconda.
@@ -11,13 +12,54 @@ if not exist %root% (
 
 set pathdir=%cd%
 
-call %root%\activate.bat
-call mkdir %pathdir%\environment
-call conda env create --prefix %pathdir%\environment\telebot-env -f telebot-env.yml
-call conda activate %pathdir%\environment\telebot-env
 
-echo Now installing model. Default model chosen: TheBloke_Wizard-Vicuna-13B-Uncensored-GPTQ
-echo Please be patient, the file is big.
-call cd %pathdir%\gptqmodels
-call git clone https://huggingface.co/TheBloke/Wizard-Vicuna-13B-Uncensored-GPTQ
+mkdir %pathdir%\environment
+call %root%\activate.bat
+if not exist %pathdir%\environment\telebot-env (
+   
+    call conda env create --prefix %pathdir%\environment\telebot-env -f telebot-env.yml
+    call conda activate %pathdir%\environment\telebot-env
+) else (  
+    echo Environment already found.
+    choice /c yn /m "Reinstall environment:  "
+    
+    if !errorlevel! equ 1 (    
+        rmdir /s /q %pathdir%\environment
+        echo Reinstalling...
+       
+        call conda env create --prefix %pathdir%\environment\telebot-env -f telebot-env.yml
+        call conda activate %pathdir%\environment\telebot-env
+
+    ) else if !errorlevel! equ 2 (
+        echo Not reinstalling.
+        
+    ) else (
+        echo Unrecognised input, not reinstalling.
+    )
+    
+)
+if not exist %pathdir%\gptqmodels\Wizard-Vicuna-13B-Uncensored-GPTQ (
+    echo Now installing model. Default model chosen: TheBloke_Wizard-Vicuna-13B-Uncensored-GPTQ
+    echo Please be patient, the file is big.
+    call cd %pathdir%\gptqmodels
+    call git clone https://huggingface.co/TheBloke/Wizard-Vicuna-13B-Uncensored-GPTQ
+
+) else (
+    echo Model already found.
+    choice /c yn /m "Reinstall model? Useful if your download is not complete/corrupted:  "
+    if !errorlevel! equ 1 ( 
+        rmdir /s /q %pathdir%\gptqmodels\Wizard-Vicuna-13B-Uncensored-GPTQ
+        echo Reinstalling...
+        echo Please be patient, the file is big.
+        call cd %pathdir%\gptqmodels
+        call git clone https://huggingface.co/TheBloke/Wizard-Vicuna-13B-Uncensored-GPTQ
+
+    ) else if !errorlevel! equ 2 (
+        echo Not reinstalling.
+    ) else (
+        echo Unrecognised command. Not reinstalling.
+    )
+
+)
+
 pause
